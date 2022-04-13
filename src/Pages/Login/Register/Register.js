e735479d5f4ed38605178fad4dd66a9dd058b445
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-04-13 20:48:37 +0600

    Complete login registration and checkout created

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const navigate = useNavigate()
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
     const handleRegister = event => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
+        createUserWithEmailAndPassword(email, password)
+        // .then(result => console.log(result))
+    }
+    if(user) {
+        navigate('/');
     }
     return (
         <div>
